134286d785b0a6cfca2e46ea071f4d31b7aadbb1
Malena de Arriba <dearribamalena@gmail.com>
2023-05-31 20:44:54 -0300

    se agrego bootstrap al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Titulo from './components/Titulo'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <>
-      
+      <Titulo></Titulo>
     </>
   )
 }
