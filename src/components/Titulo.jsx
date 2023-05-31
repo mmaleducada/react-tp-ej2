c4754d277095269b6a09cd38e84af8fcf7d6375c
Malena de Arriba <dearribamalena@gmail.com>
2023-05-31 20:41:18 -0300

    se agrego elcomponente titulo

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+const Titulo = () => {
+    return (
+        <section className="container text-center">
+            <h1><strong>Hello World!</strong></h1>
+        </section>
+    );
+};
+
+export default Titulo;
